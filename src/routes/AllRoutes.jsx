d58eb8531e05c27182f51599ca0991c5e7779fcc
Xanonymous <trusaidlin@gmail.com>
2022-01-09 23:12:52 +0800

    feat: add token verification steps before all routes

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -2,10 +2,23 @@ import {Redirect, Route, Switch} from 'react-router-dom'
 import Container from '../App/component/Container'
 import routes from './Routes'
 import {randomHash} from "../utils";
+import axios from "axios";
+
+const isValidToken = async (token) => {
+  if (!token) return false
+  try {
+    const {status} = await axios.post('http://localhost:9100/pvs-api/auth/verifyJwt', null, {
+      headers: {Authorization: token}
+    })
+    return status === 200
+  } catch (e) {
+    return false
+  }
+}
 
 function ProtectedRoute({component: Component, ...rest}) {
   const jwtToken = localStorage.getItem("jwtToken")
-  if (jwtToken === null) {
+  if (!jwtToken || !isValidToken(jwtToken)) {
     return <Redirect to="/login"/>
   }
   return (
@@ -18,6 +31,9 @@ function ProtectedRoute({component: Component, ...rest}) {
 }
 
 export default function AllRoutes() {
+  const jwt = localStorage.getItem("jwtToken")
+  if (jwt) axios.defaults.headers.common['Authorization'] = jwt
+
   return (
     <Switch>
       {routes.map((prop) =>
